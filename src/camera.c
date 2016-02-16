23aa63b6625dff782774bb68b4856be2f27d4bef
Lukasz Stanislawski <l.stanislaws@samsung.com>
2016-02-16 15:01:21 +0100

    camera: enable org.tizen.camera-app launch
    Enable launching camera application from icon on lockscreen.
    
    Change-Id: I9d3bddee1347778bf4e562241ff776ddd2809de1
    Signed-off-by: Lukasz Stanislawski <l.stanislaws@samsung.com>

---
+++ src/camera.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "camera.h"
+#include "log.h"
+
+#include <app_control.h>
+#define KEY_DISPLAY_OVER_LOCKSCREEN "http://tizen.org/lock/window/above"
+
+static void _app_control_reply_cb(app_control_h request, app_control_h reply, app_control_result_e result, void *user_data)
+{
+	switch (result) {
+		case APP_CONTROL_RESULT_APP_STARTED:
+		case APP_CONTROL_RESULT_SUCCEEDED:
+			_D("Camera application launch succcessed.");
+			break;
+		case APP_CONTROL_RESULT_FAILED:
+		case APP_CONTROL_RESULT_CANCELED:
+			_D("Camera application launch failed.");
+			break;
+	}
+	lockscreen_lcd_off_timer_set();
+}
+
+lock_error_e lock_camera_run(void)
+{
+	app_control_h app_ctr;
+
+	int err = app_control_create(&app_ctr);
+	if (err != APP_CONTROL_ERROR_NONE) {
+		_E("app_control_create failed: %s", get_error_message(err));
+		return LOCK_ERROR_FAIL;
+	}
+
+	err = app_control_set_operation(app_ctr, APP_CONTROL_OPERATION_CREATE_CONTENT);
+	if (err != APP_CONTROL_ERROR_NONE) {
+		_E("app_control_set_operation failed: %s", get_error_message(err));
+		app_control_destroy(app_ctr);
+		return LOCK_ERROR_FAIL;
+	}
+
+	err = app_control_set_launch_mode(app_ctr, APP_CONTROL_LAUNCH_MODE_GROUP);
+	if (err != APP_CONTROL_ERROR_NONE) {
+		_E("app_control_set_app_id failed: %s", get_error_message(err));
+		app_control_destroy(app_ctr);
+		return LOCK_ERROR_FAIL;
+	}
+
+	err = app_control_set_mime(app_ctr, "image/jpg");
+	if (err != APP_CONTROL_ERROR_NONE) {
+		_E("app_control_set_app_id failed: %s", get_error_message(err));
+		app_control_destroy(app_ctr);
+		return LOCK_ERROR_FAIL;
+	}
+
+	/* Send a hint to camera-app to display itself over lockscreen */
+	err = app_control_add_extra_data(app_ctr, KEY_DISPLAY_OVER_LOCKSCREEN, "on");
+	if (err != APP_CONTROL_ERROR_NONE) {
+		_E("app_control_add_extra_data failed: %s", get_error_message(err));
+		app_control_destroy(app_ctr);
+		return LOCK_ERROR_FAIL;
+	}
+
+	err = app_control_send_launch_request(app_ctr, _app_control_reply_cb, NULL);
+	if (err != APP_CONTROL_ERROR_NONE) {
+		_E("app_control_send_launch_request failed: %s", get_error_message(err));
+		app_control_destroy(app_ctr);
+		return LOCK_ERROR_FAIL;
+	}
+
+	_D("Launch request send for %s", APP_CONTROL_OPERATION_CREATE_CONTENT);
+	app_control_destroy(app_ctr);
+	lockscreen_lcd_off_timer_unset();
+
+	return LOCK_ERROR_OK;
+}
