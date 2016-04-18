45c15c34e72a014f294f7ba3eec3b5f0aa430f66
Lukasz Stanislawski <l.stanislaws@samsung.com>
2016-04-18 10:16:45 +0200

    edc changes
    Change-Id: Ie8eb00a7d87d56c657faaffe02b668bfe95f316e

--- src/main_view.c
+++ src/main_view.c
@@ -287,7 +287,7 @@ void lockscreen_main_view_time_set(Evas_Object *view, const char *locale, const 
 	elm_object_part_text_set(swipe_layout, "txt.time", time_buf);
 	elm_object_part_text_set(swipe_layout, "txt.date", str_date);
 
-	snprintf(time_buf, sizeof(time_buf), "%s %s", str_date, str_time);
+	snprintf(time_buf, sizeof(time_buf), "%s %s", str_time, str_date);
 	elm_object_part_text_set(swipe_layout, "txt.timedate", time_buf);
 
 	free(str_date);
